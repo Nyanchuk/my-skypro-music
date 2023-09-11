fc05f9b9f08a77809446328f28c1c315536ae441
global_gifts <fifa.1998.sava@mail.ru>
2023-09-11 12:58:48 +1000

    svg-картинки работаю

--- src/components/Bar/index.js
+++ src/components/Bar/index.js
@@ -1,16 +1,4 @@
-// Импорт картинок .svg
-import play from "../../img/icon/play.svg";
-import note from "../../img/icon/note.svg";
-import next from "../../img/icon/next.svg";
-import prev from "../../img/icon/prev.svg";
-import search from "../../img/icon/search.svg";
 import sprite from "../../img/icon/sprite.svg";
-import shuffle from "../../img/icon/shuffle.svg";
-import dislike from "../../img/icon/dislike.svg";
-import like from "../../img/icon/like.svg";
-import repeat from "../../img/icon/repeat.svg";
-import watch from "../../img/icon/watch.svg";
-import volume from "../../img/icon/volume.svg";
 
 const Bar = () => {
     return <div className="bar">
@@ -21,27 +9,27 @@ const Bar = () => {
               <div className="player__controls">
                 <div className="player__btn-prev">
                   <svg className="player__btn-prev-svg" alt="prev">
-                    <use xlinkHref="../../img/icon/prev.svg#icon-prev"></use>
+                    <use href={`${sprite}#icon-prev`} />
                   </svg>
                 </div>
                 <div className="player__btn-play _btn">
                   <svg className="player__btn-play-svg" alt="play">
-                    <use xlinkHref="img/icon/sprite.svg#icon-play"></use>
+                    <use href={`${sprite}#icon-play`} />
                   </svg>
                 </div>
                 <div className="player__btn-next">
                   <svg className="player__btn-next-svg" alt="next">
-                    <use xlinkHref="img/icon/sprite.svg#icon-next"></use>
+                    <use href={`${sprite}#icon-next`} />
                   </svg>
                 </div>
                 <div className="player__btn-repeat _btn-icon">
                   <svg className="player__btn-repeat-svg" alt="repeat">
-                    <use xlinkHref="img/icon/sprite.svg#icon-repeat"></use>
+                    <use href={`${sprite}#icon-repeat`} />
                   </svg>
                 </div>
                 <div className="player__btn-shuffle _btn-icon">
                   <svg className="player__btn-shuffle-svg" alt="shuffle">
-                    <use xlinkHref="img/icon/sprite.svg#icon-shuffle"></use>
+                    <use href={`${sprite}#icon-shuffle`} />
                   </svg>
                 </div>
               </div>
@@ -49,7 +37,7 @@ const Bar = () => {
                 <div className="track-play__contain">
                   <div className="track-play__image">
                     <svg className="track-play__svg" alt="music">
-                      <use xlinkHref="img/icon/sprite.svg#icon-note"></use>
+                        <use href={`${sprite}#icon-note`} />
                     </svg>
                   </div>
                   <div className="track-play__author">
@@ -62,12 +50,12 @@ const Bar = () => {
                 <div className="track-play__like-dis">
                   <div className="track-play__like _btn-icon">
                     <svg className="track-play__like-svg" alt="like">
-                      <use xlinkHref="img/icon/sprite.svg#icon-like"></use>
+                        <use href={`${sprite}#icon-like`} />
                     </svg>
                   </div>
                   <div className="track-play__dislike _btn-icon">
                     <svg className="track-play__dislike-svg" alt="dislike">
-                      <use xlinkHref="img/icon/sprite.svg#icon-dislike"></use>
+                        <use href={`${sprite}#icon-dislike`} />
                     </svg>
                   </div>
                 </div>
@@ -77,7 +65,7 @@ const Bar = () => {
               <div className="volume__content">
                 <div className="volume__image">
                   <svg className="volume__svg" alt="volume">
-                    <use xlinkHref="img/icon/sprite.svg#icon-volume"></use>
+                    <use href={`${sprite}#icon-volume`} />
                   </svg>
                 </div>
                 <div className="volume__progress _btn">
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -2,21 +2,16 @@ import playlist1 from "../../img/play1.JPEG";
 import playlist2 from "../../img/play2.JPEG";
 import playlist3 from "../../img/play3.JPEG";
 import playlist4 from "../../img/play4.JPEG";
-import login from "../../img/inlet.png";
-
+import sprite from "../../img/icon/sprite.svg";
 
 const Sidebar = () => {
     return <div className="main__sidebar sidebar">
     <div className="sidebar__personal">
       <p className="sidebar__personal-name">Войти</p>
       <div className="sidebar__icon">
-      <img
-        src={login}
-        alt="day's playlist"
-        />
-        {/* <svg alt="logout">
-          <use xlinkHref="img/icon/sprite.svg#logout"></use>
-        </svg> */}
+        <svg alt="logout">
+            <use href={`${sprite}#logout`} />
+        </svg>
       </div>
     </div>
     <div className="sidebar__block">
