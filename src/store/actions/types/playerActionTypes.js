6f3ea81330f6c1495646f8dde11dce4c5a48564e
global_gifts <fifa.1998.sava@mail.ru>
2023-10-30 16:48:03 +1000

    New Files

--- src/store/actions/types/playerActionTypes.js
+++ src/store/actions/types/playerActionTypes.js
@@ -14,3 +14,7 @@ export const SET_CURRENT_TRACK_INDEX = 'SET_CURRENT_TRACK_INDEX';
 export const GET_CURRENT_TRACK_INDEX = 'GET_CURRENT_TRACK_INDEX';
 // Для рандома треков
 export const SET_SHUFFLE = 'SET_SHUFFLE';
+// Для добавления треков в "Избранное"
+export const ADD_TRACK_FAVORITES = 'ADD_TRACK_FAVORITES';
+// Для удаления треков в "Избранное"
+export const REMOVE_TRACK_FAVORITES = 'REMOVE_TRACK_FAVORITES';
